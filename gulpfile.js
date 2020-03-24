5f261598f5dae396fa12580ed50277709ea01860
Aleksey Roshchupkin <aleks.freelance.ru@yandex.ru>
2020-03-24 05:50:41 +0300

    Feat: Add gulp plugins gulp-minify, gulp-comcat, gulp-html-replace, gulp-html-min, gulp-imagemin, gulp-clean-css, gulp-postcss, postcss-uncss. Write tasks buildHtml, buildPhp, buildCss, buildJs, buildFonts, buildImg in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,18 @@
 const syntax = 'scss';
-const {src, dest, watch} = require('gulp');
+const {src, dest, watch, series} = require('gulp');
 const sass = require('gulp-sass');
 const rename = require('gulp-rename');
 const sourcemaps = require('gulp-sourcemaps');
 const browserSync = require('browser-sync').create();
 const autoprefixer = require('gulp-autoprefixer');
+const minify = require('gulp-minify');
+const concat = require('gulp-concat');
+const htmlreplace = require('gulp-html-replace');
+const htmlmin = require('gulp-htmlmin');
+const imagemin = require('gulp-imagemin');
+const cleanCSS = require('gulp-clean-css');
+const postcss = require('gulp-postcss');
+const postUncss = require('postcss-uncss');
 
 function sync() {
   cssStyle();
@@ -18,16 +26,13 @@ function sync() {
 
 function cssStyle() {
   return src('app/' + syntax + '/**/*.' + syntax)
-  .pipe(sourcemaps.init())
-  .pipe(sass({outputStyle: 'compressed'}))
+  .pipe(sass({outputStyle: 'expanded'}))
   .pipe(autoprefixer({
     overrideBrowserslist: ['last 2 versions'],
     cascade: false
   }))
-  .pipe(rename({suffix: '.min'}))
   .pipe(sourcemaps.write('./'))
   .pipe(dest("app/css"))
-  .pipe(browserSync.stream());
 };
 
 function watchFiles() {
@@ -42,4 +47,65 @@ function reloadBrowser(done) {
   done();
 }
 
+function buildHtml(done) {
+  src('app/index.html')
+    .pipe(htmlreplace({
+        'js': 'js/main.min.js',
+        'css': 'css/main.min.css'
+    }))
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(dest('dist'));
+  done();
+}
+
+function buildPhp(done) {
+  src('app/*.php').pipe(dest('dist/'));
+  src('app/phpmailer/**/*', {dot:true}).pipe(dest('dist/phpmailer'));
+  done();
+}
+
+function buildCss(done) {
+  var plugins = [
+    postUncss({
+      html: ['app/index.html'],
+      ignore: [/\.swiper-pagination-bullet.*/, /\.modal.*/, /\.overlay.*/, /\.slider.*/]
+    })
+  ];
+  src('app/css/**/*.css')
+  .pipe(postcss(plugins))
+  .pipe(cleanCSS({
+    compatibility: 'ie8'
+  }))
+  .pipe(rename({suffix: '.min'}))
+  .pipe(dest('dist/css'));
+  done();
+}
+
+function  buildJs(done) {
+  src(['app/js/libs/*.js', 'app/js/main.js'])
+    .pipe(concat('main.js'))
+    .pipe(minify({
+      ext: {
+        min: '.min.js'
+      },
+      noSource: true
+    }))
+    .pipe(dest('dist/js'));
+  done();
+}
+
+function buildFonts(done) {
+  src('app/fonts/**/**')
+    .pipe(dest('dist/fonts'));
+  done();
+}
+
+function buildImg(done) {
+  src('app/img/**/**')
+    .pipe(imagemin({progressive: true}))
+    .pipe(dest('dist/img'));
+  done();
+}
+
 exports.default = sync;
+exports.build = series(buildHtml, buildPhp, buildCss, buildJs, buildFonts, buildImg);
